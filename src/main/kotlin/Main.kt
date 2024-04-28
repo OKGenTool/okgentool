f4e6d507216160d7ab65097aa54a395d9209a494
Hélio <heliofitas@gmail.com>
2024-04-28 17:33:50 +0100

    Align code structure with block diagram

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -10,7 +10,6 @@ fun main(args: Array<String>) {
     val client = getClient(args)
     Parser(client.sourcePath)
     Generator(client.destinationPath)
-    Generator(client.destinationPath)
 
     logger().info("Code generation ends")
 }
