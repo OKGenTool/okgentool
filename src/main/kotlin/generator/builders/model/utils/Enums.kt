39a03cf11c399f54ac5acbbd45950d684b5cc06e
Hélio <heliofitas@gmail.com>
2024-07-27 21:12:39 +0100

    Bug fix in Enums builder

--- src/main/kotlin/generator/builders/model/utils/Enums.kt
+++ src/main/kotlin/generator/builders/model/utils/Enums.kt
@@ -7,19 +7,19 @@ import datamodel.StringProperties
 import generator.capitalize
 
 fun getEnumBuilders(schema: Schema): List<TypeSpec> {
-    val enums = schema.parameters.filter { it.properties is StringProperties && it.properties.isEnum }
+    val enums = schema.parameters.filter {
+        it.properties is StringProperties
+                && it.properties.isEnum
+                && it.properties.values.isNotEmpty()
+    }
     val enumBuilders = mutableListOf<TypeSpec>()
-    schema.parameters.forEach { parameter ->
-        if (parameter.properties is StringProperties && parameter.properties.values.isNotEmpty()) {
-            enums.forEach { enum ->
-                val stringProperties = enum.properties as StringProperties
-                val enumBuilder = TypeSpec.enumBuilder(schema.simplifiedName + enum.name.capitalize())
-                stringProperties.values.forEach { enumValue ->
-                    enumBuilder.addEnumConstant(enumValue)
-                }
-                enumBuilders.add(enumBuilder.build())
-            }
+    enums.forEach { enum ->
+        val stringProperties = enum.properties as StringProperties
+        val enumBuilder = TypeSpec.enumBuilder(schema.simplifiedName + enum.name.capitalize())
+        stringProperties.values.forEach { enumValue ->
+            enumBuilder.addEnumConstant(enumValue)
         }
+        enumBuilders.add(enumBuilder.build())
     }
 
     return enumBuilders
